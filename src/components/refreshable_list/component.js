b4f3075c8592728d6521b48ff5cf9d78c8b1e038
Willem Handreck <willemhandreck@cvglobal.co>
2017-11-19 20:52:18 +1000

    Simplify `RefreshableList`
    We're not expecting this list to get very long so we can remove the
    premature optimisation logic that rendered only certain items, instead
    favouring to render the whole list at once.

--- src/components/refreshable_list/component.js
+++ src/components/refreshable_list/component.js
@@ -1,58 +1,15 @@
-import React, { Component } from 'react'
-import {
-  ListView,
-  RefreshControl,
-  Text,
-} from 'react-native'
+import React from 'react'
+import { ScrollView, RefreshControl } from 'react-native'
 
-export default class RefreshableList extends Component {
-  constructor() {
-    super()
-    this.state = {
-      dataSource: new ListView.DataSource({
-        rowHasChanged: (row1, row2) => row1 !== row2,
-      }),
-      refreshing: false,
-    }
-  }
-
-  componentDidMount() {
-    this.onNewData(this.props.items)
-  }
-
-  onNewData(data) {
-    this.setState({
-      dataSource: this.state.dataSource.cloneWithRows(data)
-    })
-  }
-
-  onRefresh() {
-    this.setState({refreshing: true})
-    this.props.onRefresh()
-      .then(items => {
-        console.log(items)
-        this.onNewData(items)
-        this.setState({refreshing: false})
-      })
-      .catch(error => {
-        console.error(error)
-        this.setState({refreshing: false})
-      })
-  }
-
-  render() {
-    return (
-      <ListView
-        dataSource={this.state.dataSource}
-        refreshControl={
-          <RefreshControl
-            colors={['#4BA9F5']}
-            refreshing={this.state.refreshing}
-            onRefresh={this.onRefresh.bind(this)}
-          />
-        }
-        renderRow={item => item}
+export default ({ items, loading, onRefresh }) => (
+  <ScrollView
+    refreshControl={
+      <RefreshControl
+        refreshing={loading}
+        onRefresh={onRefresh}
       />
-    )
-  }
-}
+    }
+  >
+    {items}
+  </ScrollView>
+)
