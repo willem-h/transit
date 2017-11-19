aab1fba41d93ca6514be8e0e069a04958d010df9
Willem Handreck <willemhandreck@cvglobal.co>
2017-11-20 08:09:29 +1000

    Fix styles to make Android work
    Something weird was happening that caused an error to throw only on
    Android. For more information see [this question](https://stackoverflow.com/questions/43476130/getting-object-freeze-can-only-be-called-on-objects-on-android).

--- src/components/card_list/component.js
+++ src/components/card_list/component.js
@@ -2,14 +2,12 @@ import React from 'react'
 
 import GoCard from '../../../providers/go_card'
 import RefreshableList from '../refreshable_list'
-import styles from './styles'
 
 const CardList = ({ loading, onRefresh }) => (
   <RefreshableList
     items={items}
     loading={loading}
     onRefresh={onRefresh}
-    style={styles}
   />
 )
 
