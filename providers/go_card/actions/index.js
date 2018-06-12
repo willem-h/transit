02c01d790730fdf5fd506572a067eee44c261cf2
Willem Handreck <willemhandreck@cvglobal.co>
2018-06-12 20:58:50 +1000

    Make GoCard provider work
    GoCard is the public transport card system used in Southeast Queensland,
    Australia. It covers Brisbane, the Sunshine Coast, and the Gold Coast.
    This is the first provider in the system. User details can't be changed
    or added yet and this is work that still needs to happen.
    
    I've been trying to think about how I can stop loading only once all the
    different providers are loaded. Considering that I've currently only got
    one provider this isn't much of an issue now. I've also decided that
    having loading finish once the first card arrives will create a snappier
    experience for users and other cards can pop into the list as they're
    loaded. We'll see how this goes and reconsider if needed.
    
    I'm adding this first because it's most useful to me.

--- providers/go_card/actions/index.js
+++
@@ -1,8 +0,0 @@
-import { createActions } from 'reduxsauce'
-
-const { Creators, Types } = createActions({
-  fetchComplete: ['balance', 'updatedAt']
-})
-
-export { Types }
-export default Creators
--- src/selectors/provider_manager.js
+++ src/selectors/provider_manager.js
@@ -1,1 +1,12 @@
-export const getLoading = ({ providerManager: { loading } }) => loading
+import { createSelector } from 'reselect'
+
+import { getGoCards } from '../../providers/go_card/selectors'
+
+const getProviderManager = ({ providerManager }) => providerManager
+
+export const getLoading = createSelector(
+  getProviderManager,
+  ({ loading }) => loading
+)
+
+export const getCards = createSelector(getProviderManager, ({ cards }) => cards)
